0971d98d61f7080e963ac66df378d59a33190330
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2017-01-08 18:45:52 -0500

    changed the time
    slowed the animation

--- ani.js
+++ ani.js
@@ -2,6 +2,6 @@
  * Created by apoorvmittal on 1/8/17.
  */
 $ (document).ready(function () {
-    $( "div" ).fadeIn( 1000 );
-    $( "nav" ).slideDown( 1500 );
+    $( "div" ).fadeIn( 3000 );
+    $( "nav" ).slideDown( 3000 );
 });
