f9ccab3c6db092b07160b1f351d7ed6096b9df79
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2017-04-16 22:38:53 -0400

    added random backgrounds

--- ani.js
+++ ani.js
@@ -1,7 +1,9 @@
 /**
  * Created by apoorvmittal on 1/8/17.
  */
+var images = ['c817012.jpg','588d7ab.jpg', '64fac45.jpg'];
 $ (document).ready(function () {
     $( "div" ).fadeIn( 3000 );
     $( "nav" ).slideDown( 3000 );
+    $('.page-bg').css({'background-image': 'url(' + images[Math.floor(Math.random() *      images.length)] + ')'});
 });
