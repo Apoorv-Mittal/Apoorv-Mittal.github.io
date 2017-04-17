8486f66d38a6887bc68a8b48e63aed3946fb3be1
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2017-04-17 12:01:51 -0400

    added the js content in the html file to fix render blocking JS

--- ani.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by apoorvmittal on 1/8/17.
- */
-var images = ['c817012.jpg','588d7ab.jpg', '64fac45.jpg'];
-$ (document).ready(function () {
-    $( "div" ).fadeIn( 2000 );
-    $( "nav" ).slideDown( 2000 );
-    $('.page-bg').css({'background-image': 'url(' + images[Math.floor(Math.random() *      images.length)] + ')'});
-});
