9dc70b874652dd4024604085f4845f6d320b4fb4
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2017-04-16 22:43:03 -0400

    changed the animation time

--- ani.js
+++ ani.js
@@ -3,7 +3,7 @@
  */
 var images = ['c817012.jpg','588d7ab.jpg', '64fac45.jpg'];
 $ (document).ready(function () {
-    $( "div" ).fadeIn( 3000 );
-    $( "nav" ).slideDown( 3000 );
+    $( "div" ).fadeIn( 2000 );
+    $( "nav" ).slideDown( 2000 );
     $('.page-bg').css({'background-image': 'url(' + images[Math.floor(Math.random() *      images.length)] + ')'});
 });
