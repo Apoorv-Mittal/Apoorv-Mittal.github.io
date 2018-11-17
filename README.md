c0345c5d2e9797e43d927779df4c6c89a9ce8119
Apoorv Mittal <apoorv@umd.edu>
2018-11-17 12:44:56 -0500

    Changed React to a different branch and the results to master to deploy to github pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# Apoorv-Mittal.github.io
-This is my personal website
---
+++ precache-manifest.88254757b2335f20d421f6eeffa160a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfe48187d41f228ad3c7",
+    "url": "/static/css/main.dc0dd277.chunk.css"
+  },
+  {
+    "revision": "bfe48187d41f228ad3c7",
+    "url": "/static/js/main.bfe48187.chunk.js"
+  },
+  {
+    "revision": "73a5b2218b74cf504818",
+    "url": "/static/js/1.73a5b221.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7ecf73d25e0d250cb37a55053b162cb",
+    "url": "/static/media/myPhoto.f7ecf73d.jpg"
+  },
+  {
+    "revision": "be2a13ec0364e8f8fc29e4884f632292",
+    "url": "/index.html"
+  }
+];
