54c26aa75b4243f8591bc8cd09e3179e60635d4a
Apoorv Mittal <apoorv@umd.edu>
2018-12-25 16:10:18 -0500

    Added Timeline

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2591517f2c737c044c144f51d766e28a.js"
+  "/precache-manifest.9bcbb22e65671902e1992862791a11d7.js"
 );
 
 workbox.clientsClaim();
