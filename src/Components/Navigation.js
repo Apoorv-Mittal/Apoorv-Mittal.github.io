8e2d177c1b34e03b01fe92d62dfa93b2c3f19ff2
Apoorv Mittal <apoorv@umd.edu>
2018-12-25 23:03:29 -0500

    Trying new way to Link

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Link } from "react-router-dom"
 
 
 class Navigation extends Component {
@@ -13,11 +14,15 @@ class Navigation extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <NavItem eventKey={1} href='/'>              
+            <NavItem eventKey={1} >  
+              <Link to={`/`} className='NavLinks'>
               HOME
+              </Link>        
             </NavItem>
-            <NavItem eventKey={3} href="/Work">
+            <NavItem eventKey={3}>
+              <Link to={`/Work`} className='NavLinks'>
               WORK EXPERIENCE
+              </Link>
             </NavItem>
             {/* <NavDropdown eventKey={4} title="ACADEMIC EXPERIENCE" id="basic-nav-dropdown">
               <MenuItem eventKey={4.1} href="/CMSC433">Programming Language Technologies and Paradigms</MenuItem>
@@ -28,8 +33,10 @@ class Navigation extends Component {
               <MenuItem divider />
               <MenuItem eventKey={4.4} href="/CMSC330">Organization of Programming Languages</MenuItem>
             </NavDropdown> */}
-            <NavItem eventKey={5} href="/Projects">
+            <NavItem eventKey={5}>
+              <Link to={`/Projects`} className='NavLinks'>
               PERSONAL PROJECTS
+              </Link>
             </NavItem>
           </Nav>
           <Nav pullRight>
