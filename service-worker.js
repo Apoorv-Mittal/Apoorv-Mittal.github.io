267bf4459a72b5081203b1f718d99088eff850d1
Apoorv Mittal <apoorv@umd.edu>
2018-12-27 18:58:57 -0500

    Added Project Description and improved fonts

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.223099850894c905336693aba62bf7e1.js"
+  "/precache-manifest.0f698a2433eaa024733c4798030d8086.js"
 );
 
 workbox.clientsClaim();
