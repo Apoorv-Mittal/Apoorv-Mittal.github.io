7be7232d11085ac9611e2071105681c2687bf9b9
Apoorv Mittal <apoorv@umd.edu>
2019-03-12 14:12:45 -0400

    Removed withTracker high order component

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6ddbfb9ac05e7ea37d1a0d2167a73c8.js"
+  "/precache-manifest.37ca2f2bb9a66b26bd60a8b766aa55da.js"
 );
 
 workbox.clientsClaim();
