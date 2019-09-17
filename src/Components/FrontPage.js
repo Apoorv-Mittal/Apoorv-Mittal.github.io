c12a964b222ec276c7fdc9cee76fda2437d0055f
Apoorv Mittal <apoorv@umd.edu>
2019-09-17 09:32:25 -0400

    Added a different type of resume and updated og resume

--- src/Components/FrontPage.js
+++ src/Components/FrontPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './myPhoto.jpg';
 import 'font-awesome/css/font-awesome.min.css';
 import resume from './Resume.pdf'
+import resumeWithCourseWork from './Resume-With-CourseWork.pdf'
 
 class Home extends Component {
   render() {
@@ -22,6 +23,9 @@ class Home extends Component {
               <i className="fa fa-linkedin fa-2x" />
             </a>
           </div>
+          <a href ={ resumeWithCourseWork } hidden>
+              <i className="fa fa-file-pdf-o fa-2x"/>
+            </a>
         </div>
     );
   }
